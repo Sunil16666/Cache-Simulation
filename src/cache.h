77b1b57003be851c67f9e255e53bebda3fa73cd1
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2024-07-03 19:18:29 +0200

    rethink simulation

--- src/cache.h
+++ src/cache.h
@@ -1,46 +1,169 @@
 #ifndef CACHE_H
 #define CACHE_H
 
+#include <list>
+#include <queue>
 #include <systemc.h>
 
 #include "cacheLine.h"
-#include "primitiveGateCountCalc.h"
 #include "simulation.h"
 
 class Cache : public sc_module {
-protected:
-    const int CYCLES;
+public:
     const unsigned CACHE_LINES;
     const unsigned CACHE_LINE_SIZE;
     const unsigned CACHE_LATENCY;
     const unsigned MEMORY_LATENCY;
-    unsigned cache_line_word_num;
-    size_t misses;
-    size_t hits;
-    size_t cycles_taken;
-    Request *requests;
-    size_t num_requests;
-    CacheLine** cache;
-    int primitiveGateCount;
+    const bool DIRECT_MAPPED = true;
+    const unsigned offset_bits = log2(CACHE_LINE_SIZE);
+    const unsigned index_bits = log2(CACHE_LINES);
+    const unsigned tag_bits = 32 - offset_bits - index_bits;
 
-public:
-    Cache(int cycles, unsigned cacheLines, unsigned CacheLineSize, unsigned cacheLatency, unsigned memoryLatency,
-          size_t num_Requests, Request requests[num_Requests]): CYCLES(cycles), CACHE_LINES(cacheLines),
-                                                                       CACHE_LINE_SIZE(CacheLineSize),
-                                                                       CACHE_LATENCY(cacheLatency),
-                                                                       MEMORY_LATENCY(memoryLatency),
-                                                                       num_requests(num_Requests) {
-        this->requests = requests;
-        cache = new CacheLine*[cacheLines];
-        cache_line_word_num = CacheLineSize / sizeof(uint32_t);
-        for (int i = 0; i < cacheLines; i++) {
-            cache[i] = new CacheLine(cache_line_word_num);
+    sc_in<bool> clk;
+    sc_in<bool> we;
+    sc_in<uint32_t> addr;
+    sc_in<uint32_t> wdata;
+    sc_out<uint32_t> rdata;
+    sc_out<bool> hit;
+
+    SC_HAS_PROCESS(Cache);
+
+    Cache(sc_module_name name, unsigned cacheLines, unsigned cacheLineSize, unsigned cacheLatency,
+          unsigned memoryLatency, bool directMapped) :
+        sc_module(name),
+        CACHE_LINES(cacheLines),
+        CACHE_LINE_SIZE(cacheLineSize),
+        CACHE_LATENCY(cacheLatency),
+        MEMORY_LATENCY(memoryLatency),
+        DIRECT_MAPPED(directMapped)
+    {
+        if (DIRECT_MAPPED) {
+            SC_METHOD(process_direct_mapped);
+        } else {
+            SC_METHOD(process_fully_associative);
         }
-        //primitiveGateCount =  PRIMITIVEGATECOUNTCALC_H.calc(CacheType, args);
+        sensitive << clk.pos();
+        cache = new CacheLine*[cacheLines];
+        for (int i = 0; i < cacheLines; i++) {
+            cache[i] = new CacheLine(cacheLineSize);
+        }
     }
 
-    virtual void process();
-    virtual Result getResult();
+private:
+    CacheLine** cache;
+    std::list<unsigned> lru_list;
+
+    void initialize_lru_list() {
+        for (unsigned i = 0; i < CACHE_LINES; ++i) {
+            lru_list.push_back(i);
+        }
+    }
+
+    void update_lru(unsigned index) {
+        lru_list.remove(index);
+        lru_list.push_back(index);
+    }
+
+    unsigned get_lru_index() {
+        return lru_list.front();
+    }
+
+    void process_direct_mapped()
+    {
+        while (true) {
+            wait(clk.posedge_event());
+            uint32_t offset = addr.read() & (1 << offset_bits) - 1;
+            uint32_t index = addr.read() >> offset_bits & (1 << index_bits) - 1;
+            uint32_t tag = addr.read() >> offset_bits + index_bits;
+
+            CacheLine* line = cache[index];
+
+            // write
+            if (we.read()) {
+                wait(CACHE_LATENCY);
+                wait(MEMORY_LATENCY); // write through
+                // hit
+                if (line->tag == tag && line->valid[offset]) {
+                    hit.write(true);
+                    mem.write(line->data[offset]);
+                    // miss
+                } else {
+                    hit.write(false);
+                    line->tag = tag;
+                    line->valid[offset] = true;
+                    line->data[offset] = wdata.read();
+                    mem.write(line->data[offset]);
+                }
+            // read
+            } else {
+                wait(CACHE_LATENCY);
+                // hit
+                if (line->tag == tag && line->valid[offset]) {
+                    hit.write(true);
+                    rdata.write(line->data[offset]);
+                    // miss
+                } else {
+                    wait(MEMORY_LATENCY);
+                    hit.write(false);
+                    rdata.write(mem.read(line->data[offset]));
+                    line->valid[offset] = true;
+                    line->tag = tag;
+                    line->data[offset] = mem.read(line->data[offset]);
+                }
+            }
+        }
+    }
+    void process_fully_associative()
+    {
+        while (true)
+        {
+            wait(clk.posedge_event());
+            uint32_t offset = addr.read() & (1 << offset_bits) - 1;
+            uint32_t tag = addr.read() >> offset_bits;
+
+            auto linePointer = std::find_if(cache, cache + CACHE_LINES, [tag, offset](CacheLine* line)
+            {
+                return line->tag == tag && line->valid[offset];
+            });
+            int lineIndex = linePointer != cache + CACHE_LINES ? linePointer - cache : -1;
+
+            if (lineIndex != -1)
+            {
+                hit.write(true);
+                if (we.read()) {
+                    wait(CACHE_LATENCY);
+                    wait(MEMORY_LATENCY); // write through
+                    cache[lineIndex]->data[offset] = wdata.read();
+                    cache[lineIndex]->valid[offset] = true;
+                    mem.write(cache[lineIndex]->data[offset]);
+                } else {
+                    wait(CACHE_LATENCY);
+                    rdata.write(cache[lineIndex]->data[offset]);
+                }
+            } else {
+                hit.write(false);
+                uint32_t lru_pointer = get_lru_index();
+                if (we.read()) {
+                    wait(CACHE_LATENCY);
+                    wait(MEMORY_LATENCY); // write through
+                    cache[lru_pointer]->tag = tag;
+                    cache[lru_pointer]->data[offset] = wdata.read();
+                    cache[lru_pointer]->valid[offset] = true;
+                    mem.write(cache[lru_pointer]->data[offset]);
+                    update_lru(lru_pointer);
+                } else {
+                    wait(CACHE_LATENCY);
+                    wait(MEMORY_LATENCY);
+                    rdata.write(mem.read(cache[lru_pointer]->data[offset]));
+                    cache[lru_pointer]->tag = tag;
+                    cache[lru_pointer]->data[offset] = mem.read(cache[lru_pointer]->data[offset]);
+                    cache[lru_pointer]->valid[offset] = true;
+                    update_lru(lru_pointer);
+                }
+            }
+        }
+    }
+
 };
 
 #endif //CACHE_H
