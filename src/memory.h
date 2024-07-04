67b7359da09f1de8d995357dbd9f8db053f9be40
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2024-07-04 12:36:11 +0200

    implement memory

--- src/memory.h
+++ src/memory.h
@@ -1,17 +1,43 @@
-//
-// Created by Linus Henn on 04.07.24.
-//
-
 #ifndef MEMORY_H
 #define MEMORY_H
+#include <map>
 #include <systemc.h>
 
-
-class Memory : sc_module {
+class Memory : public sc_module {
 public:
+    sc_in<bool> clk;
+    sc_in<bool> we;
+    sc_in<uint32_t> addr;
+    sc_in<uint32_t> wdata;
+    sc_out<uint32_t> rdata;
+
+    SC_HAS_PROCESS(Memory);
 
     Memory(sc_module_name name) : sc_module(name) {
+        SC_METHOD(process);
+        sensitive << clk.pos();
+        initialize_memory();
+    }
 
+private:
+    std::map<uint32_t, uint32_t> memory;
+
+    void initialize_memory() {
+        memory.clear();
+    }
+
+    void write(uint32_t addr, uint32_t data) {
+        memory[addr] = data;
+    }
+    void read(uint32_t addr) {
+        rdata.write(memory[addr]);
+    }
+    void process() {
+        if (we.read()) {
+            write(addr.read(), wdata.read());
+        } else {
+            read(addr.read());
+        }
     }
 };
 
