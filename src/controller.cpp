6ccca6875c1f168632b82bab17c7aa06ec2c14e6
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2024-07-04 12:29:38 +0200

    implement controller

---
+++ src/controller.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Linus Henn on 04.07.24.
+//
+
+#include "Controller.h"
---
+++ src/memory.h
@@ -0,0 +1,20 @@
+//
+// Created by Linus Henn on 04.07.24.
+//
+
+#ifndef MEMORY_H
+#define MEMORY_H
+#include <systemc.h>
+
+
+class Memory : sc_module {
+public:
+
+    Memory(sc_module_name name) : sc_module(name) {
+
+    }
+};
+
+
+
+#endif //MEMORY_H
