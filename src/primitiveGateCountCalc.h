cc7fc883120f72fbaef2615b94c0a56904e8dbb0
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2024-07-05 12:04:05 +0200

    start implementing primitveGateCount

--- src/primitiveGateCountCalc.h
+++ src/primitiveGateCountCalc.h
@@ -5,9 +5,27 @@
 
 class PrimitiveGateCountCalc {
 public:
-    static size_t primitiveGateCount() {
-        return 0; // to be implemented
+    static size_t primitiveGateCount(unsigned cacheLines, unsigned CacheLineSize, unsigned tagBits, unsigned indexBits) {
+        int muxes = tagBits * muxGateCount(cacheLines, indexBits);
     }
+
+    static int muxGateCount(unsigned cacheLines, unsigned indexBits) {
+        int not_num = indexBits;
+        int and_num = cacheLines; // (I + 1) Inputs
+        int or_num = 1; // N Inputs
+        return not_num + and_num + or_num;
+    }
+
+    static int comparatorGateCount(unsigned tagBits) {
+        int xor_num = 1 * tagBits;
+        int and_num = 1; // xor_num Inputs
+        return xor_num + and_num;
+    }
+
+    static int storageGateCount(unsigned CacheLineSize) {
+
+    }
+
 };
 
 
