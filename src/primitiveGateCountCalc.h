6dac3f9e3337f59190c3ea96fce1128056196ddc
Sunil16666 <79449240+Sunil16666@users.noreply.github.com>
2024-07-06 13:42:40 +0200

    finish primitiveGateCountCalc for directmapped Cache

--- src/primitiveGateCountCalc.h
+++ src/primitiveGateCountCalc.h
@@ -1,29 +1,36 @@
 #ifndef PRIMITIVEGATECOUNTCALC_H
 #define PRIMITIVEGATECOUNTCALC_H
-
+#include <unistd.h>
 
 
 class PrimitiveGateCountCalc {
 public:
-    static size_t primitiveGateCount(unsigned cacheLines, unsigned CacheLineSize, unsigned tagBits, unsigned indexBits) {
-        int muxes = tagBits * muxGateCount(cacheLines, indexBits);
+    static size_t primitiveGateCount(unsigned cacheLines, unsigned CacheLineSize, unsigned tagBits, unsigned indexBits, bool directMapped) {
+        int muxGateCount = tagBits * muxGateCount(cacheLines, indexBits, directMapped);
+        int comparatorGateCount = comparatorGateCount(tagBits, directMapped);
+        int storageGateCount = storageGateCount(CacheLineSize, cacheLines, tagBits, directMapped);
+
+        return muxGateCount + comparatorGateCount + storageGateCount;
     }
 
-    static int muxGateCount(unsigned cacheLines, unsigned indexBits) {
+    static int muxGateCount(unsigned cacheLines, unsigned indexBits, bool directMapped) {
         int not_num = indexBits;
         int and_num = cacheLines; // (I + 1) Inputs
         int or_num = 1; // N Inputs
         return not_num + and_num + or_num;
     }
 
-    static int comparatorGateCount(unsigned tagBits) {
+    static int comparatorGateCount(unsigned tagBits, bool directMapped) {
         int xor_num = 1 * tagBits;
         int and_num = 1; // xor_num Inputs
         return xor_num + and_num;
     }
 
-    static int storageGateCount(unsigned CacheLineSize) {
-
+    static int storageGateCount(unsigned CacheLineSize, unsigned cacheLines, unsigned tagBits, bool directMapped) {
+        int transistors_per_bit = 6; // assuming 6T SRAM Cell, and assuming transistors are primitive gates
+        int transistors_per_line = (CacheLineSize * 8 + tagBits + 1) * transistors_per_bit;
+        // 8 bits per byte, +1 for valid bit, +tagBits for tag
+        return cacheLines * transistors_per_line;
     }
 
 };
