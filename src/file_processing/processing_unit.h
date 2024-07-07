bafba9ae47271d506e271923356e11ea67ef8f4e
yannick <yannick@schuermann.org>
2024-07-07 17:54:38 +0200

    added processing unit and csv file (both empty)

--- src/file_processing/processing_unit.h
+++ src/file_processing/processing_unit.h
@@ -1,8 +1,16 @@
-//
-// Created by Yannick Schuermann on 07.07.24.
-//
-
 #ifndef PROCESSING_UNIT_H
 #define PROCESSING_UNIT_H
 
-#endif //PROCESSING_UNIT_H
+#include <string>
+#include "../simulation.h"
+
+class ProcessingUnit {
+public:
+    explicit ProcessingUnit(const std::string& csvFilePath);
+    void getRequests(size_t& numRequests, Request*& requests) const;
+
+private:
+    std::string csvFilePath;
+};
+
+#endif // PROCESSING_UNIT_H
