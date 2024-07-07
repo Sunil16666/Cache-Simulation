bef5d49f0823cc89470334a899cad55be86714ab
yannick <yannick@schuermann.org>
2024-07-07 16:19:27 +0200

    added processing unit and csv file (both empty)

---
+++ src/file_processing/processing_unit.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Yannick Schuermann on 07.07.24.
+//
+
+#include "processing_unit.h"
---
+++ src/file_processing/processing_unit.h
@@ -0,0 +1,8 @@
+//
+// Created by Yannick Schuermann on 07.07.24.
+//
+
+#ifndef PROCESSING_UNIT_H
+#define PROCESSING_UNIT_H
+
+#endif //PROCESSING_UNIT_H
